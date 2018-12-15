3fdafeeccd7f446575893110f805f833e234291b
Chayut <chayut.191@gmail.com>
2018-12-15 19:31:16 +0700

    add static run method

--- 05-Hook-into-ES6/src/03-class.js
+++ 05-Hook-into-ES6/src/03-class.js
@@ -15,6 +15,10 @@ class Person {
         console.log(`My name is ${this.name}`)
         console.log(`I'm ${this.age}'`)
     }
+
+    static run() {
+        console.log("I'm running")
+    }
 }
 
 const person = new Person('MyName', 99)
@@ -22,6 +26,7 @@ person.walk() // I'm walking...
 person.print() // My name is MyName \n I'm 99'
 
 // static method เรียกตรงผ่านคลาสได้เลย
+Person.run() // I'm running
 console.log(Person.species) // Homo sapiens sapiens
 
 // สำหรับการทำ inheritance สามารถใช้คีย์เวิร์ด extends ดังนี้
